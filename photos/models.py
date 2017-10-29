82010ffaf17b01e9894c92a9cd1ea6932546b0fb
Dan Silvers <github@silvers.net>
2017-10-29 10:13:31 -0500

    Actually resize large gallery images and use them in the templates

--- photos/models.py
+++ photos/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 
 from imagekit.models import ImageSpecField
-from imagekit.processors import ResizeToFill
+from imagekit.processors import ResizeToFill, ResizeToFit
 
 
 class Photo(models.Model):
     description = models.TextField(blank=True)
     image = models.ImageField("Gallery Image")
+
+    image_gallery = ImageSpecField(source='image',
+                                processors=[ResizeToFit(width=1200)],
+                                format='JPEG',
+                                options={'quality': 80})
 
     image_large = ImageSpecField(source='image',
                                 processors=[ResizeToFill(400, 200)],
